d0bc9b0990ba75ff83474bafcc054ddd1f7ff539
Jose Narvaez <goyox86@gmail.com>
2017-02-03 13:07:23 +0000

    Diesel now works on stable! Yay

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-#![cfg_attr(feature = "nightly", feature(proc_macro))]
-
-#[macro_use]
-extern crate diesel;
-#[cfg(feature = "nightly")]
-#[macro_use]
-extern crate diesel_codegen;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
 extern crate toml;
-
-#[cfg(feature = "nightly")]
-include!("lib.in.rs");
-
-#[cfg(feature = "with-syntex")]
-include!(concat!(env!("OUT_DIR"), "/lib.rs"));
 
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
@@ -19,6 +8,12 @@ use std::env as std_env;
 use std::str::FromStr;
 
 use app::App;
+
+pub mod schema;
+pub mod app;
+pub mod models;
+pub mod config;
+pub mod env;
 
 fn main() {
     let mut app = App::new();
