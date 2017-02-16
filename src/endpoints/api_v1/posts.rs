fb3f1b857c5641496fa90b4a1468c5e4eb9e51d7
Jose Narvaez <goyox86@gmail.com>
2017-02-16 00:25:51 +0000

    More work on Post deletion now I handle diesel NotFound and generate a response with HTTP 404.

--- src/endpoints/api_v1/posts.rs
+++ src/endpoints/api_v1/posts.rs
@@ -1,7 +1,9 @@
+use std::error::Error;
+use std::io::Cursor;
+
 use diesel::prelude::*;
 use diesel;
-use rocket::State;
-use rocket::Response;
+use rocket::{State, Request, Response, Error as RocketError};
 use rocket::http::Status;
 use rocket_contrib::{JSON, Value};
 
@@ -14,12 +16,15 @@ use schema::posts;
 #[get("/posts", format = "application/json")]
 fn api_v1_posts_index(db: State<Db>) -> JSON<Value> {
     let conn = &*db.pool().get().unwrap();
-    let results = posts.filter(published.eq(false)).load::<Post>(conn).expect("Error loading posts");
+    let results =
+        posts.filter(published.eq(false)).load::<Post>(conn).expect("Error loading posts");
     JSON(json!(results))
 }
 
 #[post("/posts", data = "<new_post>", format = "application/json")]
-fn api_v1_posts_create(db: State<Db>, new_post: JSON<NewPost>) -> Result<JSON<Post>, diesel::result::Error> {
+fn api_v1_posts_create(db: State<Db>,
+                       new_post: JSON<NewPost>)
+                       -> Result<JSON<Post>, diesel::result::Error> {
     //FIXME: Remove this unwrap
     let conn = &*db.pool().get().unwrap();
 
@@ -47,16 +52,33 @@ fn api_v1_posts_update(post_id: i32, db: State<Db>) -> JSON<Value> {
 }
 
 #[delete("/posts/<post_id>", format = "application/json")]
-fn api_v1_posts_destroy(post_id: i32, db: State<Db>) -> Result<Response, diesel::result::Error>  {
+fn api_v1_posts_destroy(post_id: i32, db: State<Db>) -> Result<Response, diesel::result::Error> {
     //FIXME: Remove this unwrap
     let conn = &*db.pool().get().unwrap();
 
-    diesel::delete(posts.find(post_id))
-        .get_result::<Post>(conn)
-        .map(|_| {
-            let mut response = Response::new();
-            response.set_status(Status::NoContent);
-            response
-        }).map_err(|err| err)
+    match diesel::delete(posts.find(post_id)).get_result::<Post>(conn) {
+        // TODO check why when I do Ok(json_response_with_status(Status::NoContent, json!({"status": "not_content"})))
+        // it fails miserably
+        Ok(_) => Ok(empty_response_with_status(Status::NoContent)),
+        Err(err) => {
+            match err {
+                diesel::result::Error::NotFound => {
+                    Ok(json_response_with_status(Status::NotFound, json!({"status": "not_found"})))
+                }
+                _ => Err(err),
+            }
+        }
+    }
 }
 
+fn empty_response_with_status<'r>(status: Status) -> Response<'r> {
+    let mut response = Response::new();
+    response.set_status(status);
+    response
+}
+
+fn json_response_with_status<'r>(status: Status, json: Value) -> Response<'r> {
+    let mut response = empty_response_with_status(status);
+    response.set_sized_body(Cursor::new(JSON(json).to_string()));
+    response
+}
